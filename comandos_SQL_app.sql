94b65fa4ca913c832468f8a6c0a2220334ffa72d
RAMIRO-GARCIA <practicas.data@cargomovil.com>
2024-05-16 18:50:55 -0600

    Se guardo los comando de sql primera parte para crear las tablas

---
+++ comandos_SQL_app.sql
@@ -0,0 +1,13 @@
+create database kigocontrolnaps;
+
+create table saleregister;
+create table depositregister;
+create table abonos;
+create table promotores;
+create table wallet;
+create table promociones;
+create table referencias;
+create table estatus;
+create table tipos;
+create table months;
+
